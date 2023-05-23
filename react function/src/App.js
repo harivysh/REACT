1cdb1fe6a7f679596e179b2055f274665e6c15ff
username <email address>
2023-05-23 11:06:42 +0530

    first react

---
+++ react function/src/App.js
@@ -0,0 +1,14 @@
+import Header from './component/Header';
+import Body from  './component/Body';
+
+function App(){
+
+    return(
+        <div>
+            <h1> App Component</h1>
+            <Header />
+            <Body />
+        </div>
+    )
+}
+export default App
---
+++ react function/src/component/Body.js
@@ -0,0 +1,10 @@
+function Body(){
+
+    return(
+        <div>
+            
+            <h4> to know about react component </h4>
+        </div>
+    )
+}
+export default Body
